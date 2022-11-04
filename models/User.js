7aecdae3aec07dc71476a6957b047b1ec9cbd53c
Greg <litm47@gmail.com>
2022-11-04 19:17:50 -0400

    completed user model

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = (email) => {
+    const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return re.test(email);
+}
+
+
+const userSchema = new Schema(
+    {
+        username: { 
+            type: String, 
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String, 
+            unique: true,
+            required: true,
+            validate: [validateEmail, 'Enter a valid email address'],
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Enter a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virutals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+.virtual('friendCounter')
+.get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema)
+
+module.exports = User;
